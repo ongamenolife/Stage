9eef76dd85ff184e5062ad481cda471d30c161ef
strawberryxiao <1034545298@qq.com>
2022-03-16 17:20:12 +0800

    creat

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <LoadBtn :text="'提交'" :load="'loading'" />
+  </div>
+</template>
+
+<script>
+import LoadBtn from "./components/loadBtn";
+export default {
+  name: "VueApp",
+  components: {
+    LoadBtn,
+  },
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/loadBtn.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="listadd" :style="btnStyle">
+      {{ loads ? `${text}` : `${load}` }}
+    </button>
+    <ul v-for="item in list" :key="item.id">
+      <li>{{ item.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loadBtn",
+  props: {
+    btnStyle: String,
+    load: String,
+    text: String,
+  },
+  data() {
+    return {
+      loads: true,
+      list: [],
+    };
+  },
+  watch: {
+    list(nVal) {
+      if (nVal.length !== 0) {
+        this.loads = true;
+      }
+    },
+  },
+  mounted() {},
+  methods: {
+    listadd() {
+      this.loads = false;
+      let List = [
+        { id: 1, text: "qwe" },
+        { id: 2, text: "asd" },
+        { id: 3, text: "zxc" },
+        { id: 4, text: "qaz" },
+      ];
+      setTimeout(() => {
+        this.list = List;
+      }, 3000);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
